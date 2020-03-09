c3f66b423cf677086305ca580fd8ce1e0612ae5c
khadijah faison <khadijahfaison@gmail.com>
2020-03-08 23:37:43 -0700

    revised

--- main.js
+++ main.js
@@ -1,81 +1,59 @@
-const arrAvg = items.reduce((a, b) => a + b.price, 0) / items.length
-console.log(arrAvg.toFixed(2))
-document.querySelector(
-  "#answer1"
-).innerHTML = `The average price is $${arrAvg.toFixed(2)}`
+let avg =
+  items.reduce(function(a, b) {
+    return a + b.price
+  }, 0) / items.length
 
-const arrItems = items.filter(function(item) {
-  return item.price > 14 && item.price < 18
-})
-console.log("Items that cost between $14.00 USD and $18.00 USD: ")
-var final = arrItems
+document.querySelector("#answer1").innerHTML = avg.toFixed(2)
+
+let itemsBetween = items
+  .filter(function(a) {
+    return a.price > 14 && a.price < 18
+  })
   .map(function(item) {
     return item.title + "\n\n"
   })
   .join("")
-document.querySelector("#answer2").innerHTML = final
+console.log(itemsBetween)
 
-var gbpItems = items.filter(function(item) {
-  return item.currency_code === "GBP"
-})
-gbpItems.forEach(function(item) {
-  console.log(item.title + " costs £" + item.price)
-})
-document.querySelector("#answer3").innerHTML = `1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18}`
+document.querySelector("#answer2").innerHTML = itemsBetween
 
-
-var woodItems = items.filter(function(item) {
-  return item.materials.indexOf("wood") > -1
-})
-var woodTitles = woodItems.map(function(item) {
-  return item.title
-})
-document.querySelector("#answer4").innerHTML = `SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
-
-Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
-
-Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
-
-Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
-
-Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.`
-
-
-var manyMaterials = items.filter(function(item) {
-  return item.materials.length > 7
-})
-
-manyMaterials.forEach(function(item){
-  document.querySelector("#answer5").innerHTML = `Qty of 2 Groomsmen Gift - Stainless Steel Personalized Bottle Opener - NO Capcatcher has 9 materials:
-  wall mount bottle opener
-  wedding
-  man cave
-  christmas gift
-  guy gift
-  fathers day
-  home bar
-  beer
-  bar
-  
-  The Three Broomsticks Customizable Beer Stein Mug, Harry Potter  Inspired, hogsmeade village, harry potter gift, three broomsticks mug  has 13 materials:
-  
-  glass
-  sandblast cabinet
-  vinyl
-  beer glass
-  pint glass
-  etched pint glass
-  etched glass
-  etched beer glass
-  16 oz pint
-  beer gift
-  etched harry potter glass
-  the three broomsticks glass
-  personalized harry potter glass`
-  
-
-  var homemade = items.filter(function(item) {
-    return item.who_made === 'i_did'
+let gbpItem = items
+  .filter(function(a) {
+    return a.currency_code === "GBP"
   })
-  console.log(homemade.length + ' were made by their sellers')
-  document.querySelector("#answer6").innerHTML = '18 were made by their sellers'
+  .map(function(items) {
+    return `${items.title} costs ${items.price}`
+  })
+
+document.querySelector("#answer3").innerHTML = gbpItem
+
+let madeOfWood = items
+  .filter(function(a) {
+    return a.materials.indexOf("wood") > -1
+  })
+  .map(function(items) {
+    return items.title + "\n\n"
+  })
+  .join("")
+
+document.querySelector("#answer4").innerHTML = madeOfWood
+
+let eightHTML = ""
+let eightOrMore = items
+  .filter(function(item) {
+    return item.materials.length >= 8
+  })
+  .forEach(function(item) {
+    eightHTML +=
+      `${item.title} has ${item.materials.length} materials\n` + "\n\n"
+    eightHTML += item.materials.join("\n")
+    eightHTML += "\n\n"
+  })
+
+document.querySelector("#answer5").innerHTML = eightHTML
+
+let made = items.filter(function(a) {
+  return a.who_made === "i_did"
+})
+document.querySelector("#answer6").innerHTML =
+  made.length + " were made by their sellers"
